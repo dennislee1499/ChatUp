ef4221b03192a1db528a4bb6f6ee4b6ce3983fcf
Dennis Lee <dennislee1499@gmail.com>
2024-07-14 19:50:26 -0400

    Exclude currently logged-in user from list of active users/ Added styling for selected user

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import Avatar from "./Avatar";
 import Logo from "./Logo";
+import { UserContext } from "./UserContext";
 
 export default function Chat() {
     const [connected, setConnected] = useState(null);
     const [activeUsers, setActiveUsers] = useState({}); 
     const [selectedUserId, setSelectedUserId] = useState(null);  
+    const { username, id } = useContext(UserContext);
 
     useEffect(() => {
         const ws = new WebSocket('ws://localhost:4000');
@@ -31,17 +33,25 @@ export default function Chat() {
         }
     }
 
+    const activeUsersExcludingSelf = {...activeUsers}; 
+    delete activeUsersExcludingSelf[id];
+
     return (
         <div className="flex h-screen">
             <div className="bg-white w-1/3">
                 <Logo />
-                {Object.keys(activeUsers).map(userId => (
+                {Object.keys(activeUsersExcludingSelf).map(userId => (
                     <div onClick={() => setSelectedUserId(userId)} 
                          key={userId} 
-                         className={"border-b border-gray-100 py-2 pl-4 flex items-center gap-2 cursor-pointer "+(userId === selectedUserId ? 'bg-blue-50' : '')}
+                         className={"border-b border-gray-100 flex items-center gap-2 cursor-pointer "+(userId === selectedUserId ? 'bg-blue-50' : '')}
                     >
-                        <Avatar username={activeUsers[userId]} userId={userId} />
-                        <span>{activeUsers[userId]}</span>
+                        {userId === selectedUserId && (
+                            <div className="w-1 h-12 bg-blue-500 rounded-r-md"></div>
+                        )}
+                        <div className="flex gap-2 py-2 pl-2 items-center">
+                            <Avatar username={activeUsers[userId]} userId={userId} />
+                            <span>{activeUsers[userId]}</span>
+                        </div>
                     </div>
                 ))}
             </div>
